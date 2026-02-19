d514164d792b86e143bd0f1d96a9935b897db1ed
Drelka <47503020+Drelka@users.noreply.github.com>
2026-02-19 13:10:41 +0000

    Add search input and click event listener

--- client/app.js
+++ client/app.js
@@ -1,13 +1,25 @@
-fetch('http://localhost:3000/weather?city=London')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    const cityName = document.querySelector('.city-name');
-    cityName.textContent = data.name;
-    const temperature = document.querySelector('.temperature');
-    temperature.textContent = data.main.temp;
-    const windSpeed = document.querySelector('.wind-speed');
-    windSpeed.textContent = data.wind.speed;
-    const icon = document.querySelector ('.icon');
-    icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  })
+fetch(`http://localhost:3000/weather?city=${citySearch}`)
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+  const cityName = document.querySelector('.city-name');
+  cityName.textContent = data.name;
+  const temperature = document.querySelector('.temperature');
+  temperature.textContent = data.main.temp;
+  const windSpeed = document.querySelector('.wind-speed');
+  windSpeed.textContent = data.wind.speed;
+  const icon = document.querySelector ('.icon');
+  icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+});
+
+const searchButton = document.querySelector('#search-button');
+const cityInput = document.querySelector('#city-input');
+
+searchButton.addEventListener ('click', citySearch);
+
+function citySearch(){
+  input = cityInput.value;
+
+  return input;
+}
+
